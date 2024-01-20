72b900dcb987e771094407bc8f0d5092586e8067
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-01-20 23:20:17 +0100

    DOC: switch to source code link button (#53)
    * MAINT: clean up `conf.py` with new Sphinx extensions
    * MAINT: remove `wiki` Sphinx role
    * MAINT: remove redundant templates
    * MAINT: sort `html_theme_options`

--- docs/conf.py
+++ docs/conf.py
@@ -1,56 +1,39 @@
 from __future__ import annotations
 
-import os
-from typing import TYPE_CHECKING
-
-from docutils import nodes
-from pybtex.plugin import register_plugin
-from pybtex.richtext import Tag, Text
-from pybtex.style.formatting.unsrt import Style as UnsrtStyle
-from pybtex.style.template import (
-    FieldIsMissing,
-    _format_list,  # pyright: ignore[reportPrivateUsage]
-    field,
-    href,
-    join,
-    node,
-    sentence,
-    words,
+from sphinx_api_relink.helpers import get_execution_mode
+from sphinx_api_relink.linkcode import (
+    _get_commit_sha,  # pyright:ignore[reportPrivateUsage]
 )
 
-if TYPE_CHECKING:
-    from sphinx.application import Sphinx
-
-# -- Project information -----------------------------------------------------
-project = "BESIII Offline Software System"
-package = "bossdoc"
+BRANCH = _get_commit_sha()
 REPO_NAME = "bossdoc"
-copyright = "2020, BESIII"
 
-
-# -- General configuration ---------------------------------------------------
-master_doc = "index.md"
-source_suffix = {
-    ".ipynb": "myst-nb",
-    ".md": "myst-nb",
-    ".rst": "restructuredtext",
+autosectionlabel_prefix_document = True
+bibtex_bibfiles = ["bibliography.bib"]
+bibtex_default_style = "unsrt_et_al"
+comments_config = {
+    "hypothesis": True,
+    "utterances": {
+        "repo": f"redeboer/{REPO_NAME}",
+        "issue-term": "pathname",
+        "label": "📝 Docs",
+    },
 }
-
-# The master toctree document.
-master_doc = "index"
-modindex_common_prefix = [
-    f"{package}.",
-]
-
+copybutton_prompt_is_regexp = True
+copybutton_prompt_text = r">>> |\.\.\. "
+copyright = "2020, BESIII"
+default_role = "py:obj"
 extensions = [
     "myst_nb",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
     "sphinx.ext.todo",
+    "sphinx_api_relink",
     "sphinx_comments",
     "sphinx_copybutton",
     "sphinx_design",
+    "sphinx_pybtex_etal_style",
     "sphinx_thebe",
     "sphinx_togglebutton",
     "sphinxcontrib.bibtex",
@@ -61,38 +44,12 @@ exclude_patterns = [
     "adr/template.md",
     "tests",
 ]
-
-# Todo-list
-todo_include_todos = True
-
-# General sphinx settings
-add_module_names = False
-autodoc_default_options = {
-    "members": True,
-    "undoc-members": True,
-    "show-inheritance": True,
-    "special-members": ", ".join([
-        "__call__",
-        "__eq__",
-    ]),
-}
-graphviz_output_format = "svg"
-html_copy_source = True  # needed for download notebook button
 html_favicon = "_static/favicon.ico"
 html_logo = "https://github.com/redeboer/bossdoc/assets/29308176/71ae5632-3aa9-4756-b4bb-8af397c62951"
 html_show_copyright = False
-html_show_sourcelink = False
-html_show_sphinx = False
 html_sourcelink_suffix = ""
 html_theme = "sphinx_book_theme"
 html_theme_options = {
-    "repository_url": f"https://github.com/redeboer/{REPO_NAME}",
-    "repository_branch": "main",
-    "path_to_docs": "docs",
-    "use_download_button": True,
-    "use_edit_page_button": True,
-    "use_issues_button": True,
-    "use_repository_button": True,
     "launch_buttons": {
         "binderhub_url": "https://mybinder.org",
         "colab_url": "https://colab.research.google.com",
@@ -100,39 +57,21 @@ html_theme_options = {
         "thebe": True,
         "thebelab": True,
     },
+    "path_to_docs": "docs",
+    "repository_url": f"https://github.com/redeboer/{REPO_NAME}",
+    "repository_branch": BRANCH,
     "show_navbar_depth": 2,
     "show_toc_level": 2,
+    "use_download_button": False,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "use_repository_button": True,
+    "use_source_button": True,
 }
 html_title = "BOSS Documentation"
-panels_add_bootstrap_css = False  # remove panels css to get wider main content
-pygments_style = "sphinx"
-todo_include_todos = False
-viewcode_follow_imported_members = True
-
-# Cross-referencing configuration
-default_role = "py:obj"
-primary_domain = "py"
-nitpicky = True  # warn if cross-references are missing
-
-# Intersphinx settings
 intersphinx_mapping = {
     "compwa": ("https://compwa.github.io", None),
 }
-
-# Settings for autosectionlabel
-autosectionlabel_prefix_document = True
-
-# Settings for bibtex
-bibtex_bibfiles = ["bibliography.bib"]
-suppress_warnings = [
-    "myst.domains",
-]
-
-# Settings for copybutton
-copybutton_prompt_is_regexp = True
-copybutton_prompt_text = r">>> |\.\.\. "  # doctest
-
-# Settings for linkcheck
 linkcheck_anchors = False
 linkcheck_ignore = [
     "http://code.ihep.ac.cn/redeboer/IniSelect",
@@ -144,19 +83,6 @@ linkcheck_ignore = [
     r"http://[A-Za-z0-9]+\.ucas\.ac\.cn",
     r"https://[A-Za-z0-9]+\.ihep\.ac\.cn",
 ]
-
-# Settings for myst_nb
-nb_execution_timeout = -1
-nb_output_stderr = "remove"
-
-nb_execution_mode = "off"
-EXECUTE_NB = False
-if "EXECUTE_NB" in os.environ:
-    print("\033[93;1mWill run Jupyter notebooks!\033[0m")
-    EXECUTE_NB = True  # pyright: ignore[reportConstantRedefinition]
-    nb_execution_mode = "cache"
-
-# Settings for myst-parser
 myst_enable_extensions = [
     "amsmath",
     "colon_fence",
@@ -165,116 +91,19 @@ myst_enable_extensions = [
     "smartquotes",
 ]
 myst_update_mathjax = False
-
-# Settings for sphinx_comments
-comments_config = {
-    "hypothesis": True,
-    "utterances": {
-        "repo": f"redeboer/{REPO_NAME}",
-        "issue-term": "pathname",
-        "label": "📝 Docs",
-    },
-}
-
-# Settings for Thebe cell output
+nb_execution_mode = get_execution_mode()
+nb_execution_timeout = -1
+nb_output_stderr = "remove"
+nitpicky = True  # warn if cross-references are missing
+panels_add_bootstrap_css = False  # remove panels css to get wider main content
+primary_domain = "py"
+project = "BESIII Offline Software System"
+pygments_style = "sphinx"
 thebe_config = {
     "repository_url": html_theme_options["repository_url"],
     "repository_branch": html_theme_options["repository_branch"],
 }
-
-
-# Add roles to simplify external linnks
-def setup(app: Sphinx):
-    app.add_role("wiki", autolink("https://en.wikipedia.org/wiki/%s", {"_": " "}))
-
-
-def autolink(pattern: str, replace_mapping: dict[str, str]):
-    def role(name, rawtext, text: str, lineno, inliner, options={}, content=[]):
-        output_text = text
-        for search, replace in replace_mapping.items():
-            output_text = output_text.replace(search, replace)
-        url = pattern % (text,)
-        node = nodes.reference(rawtext, output_text, refuri=url, **options)
-        return [node], []
-
-    return role
-
-
-# Specify bibliography style
-@node
-def et_al(children, data, sep="", sep2=None, last_sep=None):
-    if sep2 is None:
-        sep2 = sep
-    if last_sep is None:
-        last_sep = sep
-    parts = [part for part in _format_list(children, data) if part]
-    if len(parts) <= 1:
-        return Text(*parts)
-    if len(parts) == 2:  # noqa: PLR2004
-        return Text(sep2).join(parts)
-    if len(parts) == 3:  # noqa: PLR2004
-        return Text(last_sep).join([Text(sep).join(parts[:-1]), parts[-1]])
-    return Text(parts[0], Tag("em", " et al"))
-
-
-@node
-def names(children, context, role, **kwargs):
-    """Return formatted names."""
-    assert not children
-    try:
-        persons = context["entry"].persons[role]
-    except KeyError:
-        raise FieldIsMissing(role, context["entry"]) from None
-    style = context["style"]
-    formatted_names = [
-        style.format_name(person, style.abbreviate_names) for person in persons
-    ]
-    return et_al(**kwargs)[formatted_names].format_data(context)
-
-
-class MyStyle(UnsrtStyle):
-    def __init__(self):
-        super().__init__(abbreviate_names=True)
-
-    def format_names(self, role, as_sentence=True):
-        formatted_names = names(role, sep=", ", sep2=" and ", last_sep=", and ")
-        if as_sentence:
-            return sentence[formatted_names]
-        return formatted_names
-
-    def format_url(self, e):
-        return words[
-            href[
-                field("url", raw=True),
-                field("url", raw=True, apply_func=remove_http),
-            ]
-        ]
-
-    def format_isbn(self, e):
-        return href[
-            join[
-                "https://isbnsearch.org/isbn/",
-                field("isbn", raw=True, apply_func=remove_dashes_and_spaces),
-            ],
-            join[
-                "ISBN:",
-                field("isbn", raw=True),
-            ],
-        ]
-
-
-def remove_dashes_and_spaces(isbn: str) -> str:
-    to_remove = ["-", " "]
-    for remove in to_remove:
-        isbn = isbn.replace(remove, "")
-    return isbn
-
-
-def remove_http(input: str) -> str:
-    to_remove = ["https://", "http://"]
-    for remove in to_remove:
-        input = input.replace(remove, "")
-    return input
-
-
-register_plugin("pybtex.style.formatting", "unsrt_et_al", MyStyle)
+todo_include_todos = True
+suppress_warnings = [
+    "myst.domains",
+]
