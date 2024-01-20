5475c23d7c1f16bf58874698079201b98ee762d1
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-01-20 22:58:21 +0100

    MAINT: clean up `conf.py` with new Sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -1,35 +1,12 @@
 from __future__ import annotations
 
-import os
+from sphinx_api_relink.helpers import get_execution_mode
 
-from pybtex.plugin import register_plugin
-from pybtex.richtext import Tag, Text
-from pybtex.style.formatting.unsrt import Style as UnsrtStyle
-from pybtex.style.template import (
-    FieldIsMissing,
-    _format_list,  # pyright: ignore[reportPrivateUsage]
-    field,
-    href,
-    join,
-    node,
-    sentence,
-    words,
-)
-
-
-def get_execution_mode() -> str:
-    if "EXECUTE_NB" in os.environ:
-        print("\033[93;1mWill run Jupyter notebooks!\033[0m")
-        return "cache"
-    return "off"
-
-
-PACKAGE_NAME = "bossdoc"
 REPO_NAME = "bossdoc"
-
 
 autosectionlabel_prefix_document = True
 bibtex_bibfiles = ["bibliography.bib"]
+bibtex_default_style = "unsrt_et_al"
 comments_config = {
     "hypothesis": True,
     "utterances": {
@@ -51,6 +28,7 @@ extensions = [
     "sphinx_comments",
     "sphinx_copybutton",
     "sphinx_design",
+    "sphinx_pybtex_etal_style",
     "sphinx_thebe",
     "sphinx_togglebutton",
     "sphinxcontrib.bibtex",
@@ -118,11 +96,6 @@ panels_add_bootstrap_css = False  # remove panels css to get wider main content
 primary_domain = "py"
 project = "BESIII Offline Software System"
 pygments_style = "sphinx"
-source_suffix = {
-    ".ipynb": "myst-nb",
-    ".md": "myst-nb",
-    ".rst": "restructuredtext",
-}
 thebe_config = {
     "repository_url": html_theme_options["repository_url"],
     "repository_branch": html_theme_options["repository_branch"],
@@ -132,83 +105,3 @@ viewcode_follow_imported_members = True
 suppress_warnings = [
     "myst.domains",
 ]
-
-
-# Specify bibliography style
-@node
-def et_al(children, data, sep="", sep2=None, last_sep=None):
-    if sep2 is None:
-        sep2 = sep
-    if last_sep is None:
-        last_sep = sep
-    parts = [part for part in _format_list(children, data) if part]
-    if len(parts) <= 1:
-        return Text(*parts)
-    if len(parts) == 2:  # noqa: PLR2004
-        return Text(sep2).join(parts)
-    if len(parts) == 3:  # noqa: PLR2004
-        return Text(last_sep).join([Text(sep).join(parts[:-1]), parts[-1]])
-    return Text(parts[0], Tag("em", " et al"))
-
-
-@node
-def names(children, context, role, **kwargs):
-    """Return formatted names."""
-    assert not children
-    try:
-        persons = context["entry"].persons[role]
-    except KeyError:
-        raise FieldIsMissing(role, context["entry"]) from None
-    style = context["style"]
-    formatted_names = [
-        style.format_name(person, style.abbreviate_names) for person in persons
-    ]
-    return et_al(**kwargs)[formatted_names].format_data(context)
-
-
-class MyStyle(UnsrtStyle):
-    def __init__(self):
-        super().__init__(abbreviate_names=True)
-
-    def format_names(self, role, as_sentence=True):
-        formatted_names = names(role, sep=", ", sep2=" and ", last_sep=", and ")
-        if as_sentence:
-            return sentence[formatted_names]
-        return formatted_names
-
-    def format_url(self, e):
-        return words[
-            href[
-                field("url", raw=True),
-                field("url", raw=True, apply_func=remove_http),
-            ]
-        ]
-
-    def format_isbn(self, e):
-        return href[
-            join[
-                "https://isbnsearch.org/isbn/",
-                field("isbn", raw=True, apply_func=remove_dashes_and_spaces),
-            ],
-            join[
-                "ISBN:",
-                field("isbn", raw=True),
-            ],
-        ]
-
-
-def remove_dashes_and_spaces(isbn: str) -> str:
-    to_remove = ["-", " "]
-    for remove in to_remove:
-        isbn = isbn.replace(remove, "")
-    return isbn
-
-
-def remove_http(input: str) -> str:
-    to_remove = ["https://", "http://"]
-    for remove in to_remove:
-        input = input.replace(remove, "")
-    return input
-
-
-register_plugin("pybtex.style.formatting", "unsrt_et_al", MyStyle)
